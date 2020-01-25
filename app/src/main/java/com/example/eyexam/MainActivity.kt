c69eab3ba658379b23b2298574f9a93831284346
CorrectButNotRight <42951909+CorrectButNotRight@users.noreply.github.com>
2020-01-25 16:56:54 -0500

    activity main layout

--- app/src/main/java/com/example/eyexam/MainActivity.kt
+++ app/src/main/java/com/example/eyexam/MainActivity.kt
@@ -13,14 +13,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
+        //setSupportActionBar(toolbar)
+/*
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
-        }
-    }
+        }*/
 
+    }
+/*
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -35,5 +36,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
+    }*/
 }
